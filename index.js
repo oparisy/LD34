cf8882fd8d1b806fd2bd207b61e1be609519136f
oparisy <olivier.parisy@gmail.com>
2015-12-14 02:08:19 +0100

    Can (strangely) plant trees

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ var vec3    = require('gl-vec3');
 var createOrbitCamera = require("orbit-camera");
 var GPControls = require('gp-controls');
 
+var intersect = require('ray-triangle-intersection');
+var computeNormals = require('normals');
+
 var gamepad = GPControls({
     '<axis-left-x>': 'x',
 	'<axis-left-y>': 'y',
@@ -60,6 +63,9 @@ console.log('WebGL setup done, loading assets...');
 var globe = null, cloud = null;
 loadAssets();
 
+var tree;
+var animation;
+
 function loadAssets(globeLoaded) {
 
 	// Load globe
@@ -88,6 +94,8 @@ function loadAssets(globeLoaded) {
 		cloud = new Model(objAndMtl, gl);
 		cloud.setup(cloud.geom.data.rawVertices);
 	});
+	
+	loadTree();
 }
 
 var cloudsPos = [
@@ -99,7 +107,9 @@ var cloudsPos = [
 	[ +8, 12, -2 ]
 ];
 
-var count = 0;
+var treePos = [];
+
+var lastPlant = 0;
 
 var lastDate = Date.now();
 var lastx=0, lasty=0;
@@ -122,11 +132,10 @@ function render() {
 	var proj = mat4.create();
 	mat4.perspective(proj, Math.PI / 4, width / height, 0.001, 1000);
 
-	// update camera rotation angle
-	//camera.rotation = Date.now() * 0.0004;
-
-	if (globe !== null && count < 20) {
+	if (globe !== null) {
 	
+		var view = camera.view();
+
 		gamepad.poll();
 		if (gamepad.enabled) {
 			
@@ -136,7 +145,47 @@ function render() {
 			lasty = y;
 			
 			if (gamepad.inputs.plant.pressed) {
-				console.log('Plant!');
+			
+				// Not more than once per second
+				if (Date.now() - lastPlant > 500) {
+					lastPlant = Date.now();
+
+					console.log('Plant!');
+					
+					var pt = [0, 0, 0];
+					
+					var invView = mat4.create();
+					mat4.invert(invView, view);
+					
+					var dir = vec3.create();
+					//vec3.set(dir, eye[0], eye[1], eye[2]);
+					var modelDir = vec3.create();
+					vec3.transformMat4(modelDir, dir, invView);
+					console.log('modelDir:', modelDir);
+					vec3.scale(modelDir, modelDir, -0.5);
+					
+					// Search for an intersection with a globe triangle
+					var data = globe.geom.data;
+					var faces = data.rawFaces;
+					var intersection = null;
+					for (var f=0; f<faces.length; f++) {
+						var i0 = faces[f][0], i1 = faces[f][1], i2 = faces[f][2];
+						//console.log('i0:',i0,'i1:',i1,'i2:',i2);
+						var tri = [ data.rawVertices[i0], data.rawVertices[i1], data.rawVertices[i2] ];
+						//console.log('pt:',pt);
+						//console.log('modelDir',modelDir);
+						//console.log('tri:',tri);
+						intersection = intersect([], pt, modelDir, tri);
+						if (intersection !== null) {
+							break;
+						}
+					}
+					
+					if (intersection !== null) {
+						console.log('Intersection found:', intersection);
+						treePos.push({'pos': intersection, 'frame': 0});
+					}
+				}
 			}
 		}
 		
@@ -145,7 +194,6 @@ function render() {
 
 		// Compute matrices
 		// See http://stackoverflow.com/a/21079741/38096
-		var view = camera.view();
 		var normalMatrix = mat4.create();
 		var temp = mat4.create();
 		mat4.invert(temp, view);
@@ -173,9 +221,118 @@ function render() {
 			vec3.set(vpos, posarr[0], posarr[1], posarr[2]);
 			mat4.translate(cloudModel, mat4.create(), vpos);
 			shader.uniforms.model = cloudModel;
-			console.log(cloudModel);
 			cloud.draw(shader);
 		}
 		cloud.geom.unbind();
+		
+		if (tree !== null && treePos.length > 0) {
+			
+			for (var tr=0; tr<treePos.length; tr++) {
+
+				var data = tree.geom.data;
+
+				// Compute current animation state
+				var currentFrame = animation.length - 1;
+				/*
+				var currentFrame = treePos[tr].frame;//Math.floor(Date.now() * 0.02) % animation.length;
+				if (currentFrame < animation.length - 1) {
+					treePos[tr].frame++;
+				}
+				*/
+				var currentFrameVertices = animation[currentFrame];
+				assert.isArray(currentFrameVertices);
+				assert.equal(currentFrameVertices.length, data.baseVertices.length);
+
+				// Duplicate positions (see comments in onModelLoaded)
+				var positions = [];
+				for (var j=0; j<data.rawFaces.length; j++) {
+					var idx = data.rawFaces[j];
+					positions.push(currentFrameVertices[idx[0]]);
+					positions.push(currentFrameVertices[idx[1]]);
+					positions.push(currentFrameVertices[idx[2]]);
+				}
+				
+				// Compute normals per vertex
+				var faceNormals = computeNormals.faceNormals(data.faceIndices, positions);
+				var normals = [];
+				for (var k=0; k<data.rawFaces.length; k++) {
+					var faceNormal = faceNormals[k];
+					normals.push(faceNormal);
+					normals.push(faceNormal);
+					normals.push(faceNormal);
+				}
+
+				// Update model with animation data
+				// Stupidly inefficient but I won't recode this now :)
+				tree.geom.dispose();
+				tree.geom = Geom(gl).attr('position', positions).attr('normal', normals).faces(data.faceIndices);
+				tree.geom.data = data;
+
+				tree.geom.bind(shader);
+				shader.uniforms.proj = proj;
+				shader.uniforms.view = view;
+				shader.uniforms.normalMatrix = normalMatrix;
+
+				var treeModel = mat4.create();
+				var posarr = treePos[tr].pos;
+				var vpos = vec3.create();
+				vec3.set(vpos, posarr[0], posarr[1], posarr[2]);
+				mat4.translate(treeModel, mat4.create(), vpos);
+				shader.uniforms.model = treeModel;
+				tree.draw(shader);
+
+				tree.geom.unbind();
+			}
+		}
 	}
+}
+
+function loadTree() {
+
+	var objLoader = new ObjMtlLoader();
+	objLoader.load('./assets/palmtree_animated_joined.obj', './assets/palmtree_animated_joined.mtl', function(err, objAndMtl) {
+		if (err) {
+			console.log(err);
+			throw err;
+		}
+		
+		console.log('Tree model loaded');
+
+		tree = new Model(objAndMtl, gl);
+		tree.setup(tree.geom.data.rawVertices);
+	});
+
+	var pc2Loader = new PC2Loader();
+	pc2Loader.on('readable', function() {
+		var pc2Data = pc2Loader.read();
+		if (pc2Data) {
+			console.log('Tree animation loaded');
+			
+			// Convert PC2 animation data
+			animation = [];
+			for (var i=0; i<pc2Data.frames.length; i++) {
+				var frame = [];
+				var points = pc2Data.frames[i].points;
+				for (var j=0; j<points.length; j++) {
+					frame.push([ points[j].x, points[j].y, points[j].z ]);
+				}
+				animation.push(frame);
+			}
+		}
+	});
+
+	// binary-xhr works, too
+	request('./assets/palmtree_animated_joined.pc2', {
+		responseType: 'arraybuffer'
+	}, function (err, data) {
+		if (err) {
+			console.log(err);
+			throw err;
+		}
+
+		// Cannot consume ArrayBuffer content directly...
+		// See http://www.html5rocks.com/en/tutorials/webgl/typed_arrays/
+		var dataBuffer = arrayBufferToBuffer(data);
+		pc2Loader.write(dataBuffer);
+	});
 }
